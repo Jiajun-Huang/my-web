b8b2e2fa992535b97d2c0f97e3939f5a5b7c6128
Jiajun Huang <jhuang430@qq.com>
2022-11-12 02:04:55 -0800

    add some basic class

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <h1>footer</h1>
+    </div>
+  )
+}
+
+export default Footer   
---
+++ src/page/aboutme/Aboutme.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Aboutme() {
+  return (
+    <div>
+      <h1>About xcvbme</h1>
+    </div>
+  )
+}
+
+const styles = StyleSheet.create({})    
