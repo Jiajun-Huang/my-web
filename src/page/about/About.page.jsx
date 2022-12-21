f128e2cf1915a8d64fc22a0955e1451fc4c779f6
Jiajun Huang <jhuang430@qq.com>
2022-12-21 14:59:43 -0800

    add firebase.util

---
+++ src/page/about/About.page.jsx
@@ -0,0 +1,29 @@
+import React, { Component, useEffect } from "react";
+
+//componement
+import MarkDown from "../../util/Markdown/Markdown.tsx";
+import Card from "../../component/card/Card";
+import Title from "../../component/title/Title.component.tsx";
+
+import content from "./about.md";
+import { useState } from "react";
+
+export default function About() {
+  const [md, setMd] = useState("");
+  useEffect(() => {
+    fetch(content)
+      .then((r) => r.text())
+      .then((text) => {
+        setMd(text);
+        //console.log(text);
+      });
+  });
+  return (
+    <div>
+      <Title>About</Title>
+      <Card type="main-card">
+        <MarkDown>{md}</MarkDown>
+      </Card>
+    </div>
+  );
+}
